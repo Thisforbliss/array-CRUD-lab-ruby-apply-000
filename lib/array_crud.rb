d7e611fe9d9fecf1fc8e4e71b19d673b2fa62cbf
Malcolm Leake <echelon1089@gmail.com>
2018-03-13 22:00:45 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,9 +12,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array =  ["mal", "colm", "is", "cool"]
-  array.unshift("speed")
-
+ 
 end
 
 def remove_element_from_end_of_array(array)
